74bc8c37a2ac306b134c914de2c84064da9f3394
Olena Kaliuzhna <om.kaliuzhna.dz@gmail.com>
2021-08-10 12:35:18 +0300

    adds adaptive

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 5,
-  slidesPerGroup: 5,
-  spaceBetween: 71,
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
 
-  loop: true,
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
+
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 60,
+
+  // loop: true,
   freeMode: true,
   pagination: {
     el: ".swiper-pagination",
@@ -15,15 +30,23 @@ const swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
+    480: {
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+    },
+    767: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
     1200: {
       slidesPerView: 4,
       slidesPerGroup: 4,
-      spaceBetween: 50,
+      // spaceBetween: 50,
     },
     1600: {
       slidesPerView: 5,
       slidesPerGroup: 5,
-      spaceBetween: 71,
+      spaceBetween: 25,
     },
   },
 });
